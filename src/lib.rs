b113c50d60846b324a19e953250fcdb9286c0883
Magic Len <len@magiclen.org>
2025-12-13 22:55:35 +0800

    update project

--- src/lib.rs
+++ src/lib.rs
@@ -48,7 +48,8 @@ default-features = false
 */
 
 #![cfg_attr(not(feature = "std"), no_std)]
-#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+#![allow(unexpected_cfgs)]
+#![cfg_attr(docsrs_1_92, feature(doc_cfg))]
 
 pub extern crate chrono;
 
